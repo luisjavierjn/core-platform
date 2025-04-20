d7150c652e7dc8f71eff9a9fa69b997e9e3a9542
Luis Jimenez <luisjjn@ext.inditex.com>
2025-04-20 13:44:04 +0200

    inserted data

--- src/main/java/com/inditex/coreplatform/controllers/PriceController.java
+++ src/main/java/com/inditex/coreplatform/controllers/PriceController.java
@@ -1,9 +1,23 @@
 package com.inditex.coreplatform.controllers;
 
+import com.inditex.coreplatform.domain.dto.PriceDTO;
+import com.inditex.coreplatform.services.PriceServices;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class PriceController {
+  private final PriceServices priceServices;
+
+  @GetMapping("/all")
+  public ResponseEntity<List<PriceDTO>> getAllPrices() {
+    return ResponseEntity.ok(this.priceServices.getAllPrices());
+  }
 }
--- src/main/java/com/inditex/coreplatform/services/PriceServices.java
+++ src/main/java/com/inditex/coreplatform/services/PriceServices.java
@@ -1,9 +1,18 @@
 package com.inditex.coreplatform.services;
 
+import com.inditex.coreplatform.domain.dto.PriceDTO;
+import com.inditex.coreplatform.repositories.PriceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PriceServices {
+  private final PriceRepository priceRepository;
+
+  public List<PriceDTO> getAllPrices() {
+    return null;
+  }
 }
